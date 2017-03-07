a3f38c8f3c26302f258d3596eb8bdd2870021bad
Fernando Aleman <fernandoaleman@mac.com>
2017-03-07 09:39:20 -0600

    Updated sleep command in check.rb and removed from codeship-steps.yml

--- check.rb
+++ check.rb
@@ -11,7 +11,8 @@ puts "Redis"
 redis = Redis.new(host: "redis")
 puts "REDIS VERSION: #{redis.info["redis_version"]}"
 
-sleep 4
+sleep 20
+puts "Postgres"
 postgres_username = "postgres"
 postgres_password = ""
 test = PG.connect("postgres", 5432, "", "", "postgres", postgres_username, postgres_password)
